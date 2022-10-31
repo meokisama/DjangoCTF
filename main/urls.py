96383347a9b14bb1b5ef7eff96d1544efba2a944
chien <chienpro987654@gmail.com>
2022-10-31 20:37:15 +0700

    add login

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,15 @@
+from unicodedata import name
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('challenge/<str:pk>/',views.challenge,name="challenge"),
+    path('challenge',views.challenge,name="challenge"),
     path('calendar',views.calendar,name="calendar"),
     path('chart',views.chart,name="chart"),
     path('news',views.news,name="news"),
-    path('community',views.community,name="community")
+    path('community',views.community,name="community"),
+    path('user_login',views.user_login,name="user_login"),
+    path('user_logout',views.user_logout,name="user_logout")
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
-from multiprocessing import context
+from telnetlib import LOGOUT
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from tkinter.tix import CheckList
 from urllib import request
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 from .models import Challenge
 
@@ -13,14 +15,14 @@ def home(request):
     context = {'challenges':challenges}
     return render(request, 'index.html',context)
 
-def challenge(request, pk):
-    challenge = Challenge.objects.get(id=pk)
+def challenge(request):
+    # challenge = Challenge.objects.get(id=pk)
     # challenge = None
     # for i in challenges:
     #     if i['id']==int(pk):
     #       challenge=i
-    context = {'challenge':challenge}
-    return render(request, './menu/challenge.html',context)
+    # context = {'challenge':challenge}
+    return render(request, './menu/challenge.html')
 
 def calendar(request):
     return render(request, 'menu/calendar.html')
@@ -33,3 +35,32 @@ def news(request):
 
 def community(request):
     return render(request, 'menu/community.html')
+
+def user_login(request):
+
+    username=None
+    password=None
+
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+    try:
+        user = User.object.get(username=username)
+    except:
+        messages.error(request,'Username does not exist')
+
+    user = authenticate(request,username=username,password=password)
+
+    if user is not None:
+        login(request,user)
+        return redirect('home')
+    else:
+        messages.error(request,'Invalid Username or Password')
+    
+    context={}
+    return render(request, 'menu/login_register.html',context)
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
