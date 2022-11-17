347d8d3e722128e6cca2ef7421f414e18dcffe18
meokisama <hoangdinhsang123@gmail.com>
2022-11-17 14:02:31 +0700

    add 2 more charts

--- chart/views.py
+++ chart/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from community.models import Topic, Post
+from main.models import Challenge
 
 # Create your views here.
 def chart(request):
     topics = Topic.objects.all()
     data_post = []
     data_challenge = []
+
     for topic in topics:
         data_post.append(topic.post_set.all().count())
         # data_challenge.append(topic.challenge_set.all().count())
-    data_challenge = [5, 4, 1, 3]
+    
+    challenges = Challenge.objects.all()
+
+    data_challenge = [5, 4, 1, 3, 1]
     context = {'topics':topics, 'data_post':data_post, 'data_challenge':data_challenge}
     return render(request, 'chart/chart.html', context)
