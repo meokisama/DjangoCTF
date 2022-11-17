4da6a92a08009e43d44c08c24e5e10357935f055
meokisama <hoangdinhsang123@gmail.com>
2022-11-17 10:53:19 +0700

    Chart.js

--- chart/views.py
+++ chart/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from community.models import Topic
+from community.models import Topic, Post
 
 # Create your views here.
 def chart(request):
     topics = Topic.objects.all()
-    context = {'topics':topics}
+    data_post = []
+    data_challenge = []
+    for topic in topics:
+        data_post.append(topic.post_set.all().count())
+        # data_challenge.append(topic.challenge_set.all().count())
+    data_challenge = [5, 4, 1, 3]
+    context = {'topics':topics, 'data_post':data_post, 'data_challenge':data_challenge}
     return render(request, 'chart/chart.html', context)
