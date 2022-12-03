bc6cd5670aa6a5501a79293784dbfb76fda6e8d9
chien <chienpro987654@gmail.com>
2022-12-03 16:48:15 +0700

    Create quizz for challenge (aboriginal)

--- challenge/urls.py
+++ challenge/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.challenge, name="challenge"),
     path('create_challenge',views.create_challenge,name="create_challenge"),
+    path('create_challenge_quizz/<str:pk>/',views.create_challenge_quizz,name="create_challenge_quizz"),
 ]
--- challenge/views.py
+++ challenge/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import ChallengeForm
@@ -13,18 +13,30 @@ def challenge(request):
 def create_challenge(request):
 
     form = ChallengeForm()
-    name = request.POST.get('name')
-    date_start = request.POST.get('date_start')
-    date_end = request.POST.get('date_end')
-    description = request.POST.get('description')
     if request.method == 'POST':
-        challenge = Challenge.objects.create(
-            name = name,
-            date_start = date_start,
-            date_end = date_end,
-            description = description
-        )
-        challenge.save()
+        form = ChallengeForm(request.POST)
+        if form.is_valid():
+            name = request.POST.get('name')
+            date_start = request.POST.get('date_start')
+            date_end = request.POST.get('date_end')
+            description = request.POST.get('description')
+
+            challenge = Challenge.objects.create(
+                name = name,
+                date_start = date_start,
+                date_end = date_end,
+                description = description
+            )
+            challenge.save()
+            print(challenge.id)
+            return redirect(f'create_challenge_quizz/{challenge.id}')
 
     context={'form':form}
     return render(request,'create_challenge.html',context)
+
+@login_required(login_url='login')
+def create_challenge_quizz(request, pk):
+    challenge = Challenge.objects.get(id=pk)
+    name = challenge.name
+    context= {'name':name}
+    return render(request,'create_challenge_quizz.html',context)
